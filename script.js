2f1aae8e257eefeb4ba5a0a43cf02d5c6c154cf5
nruffilo <nickruffilo@gmail.com>
2015-11-24 23:33:41 -0500

    Initial commit

--- script.js
+++ script.js
@@ -12,15 +12,9 @@ var hero_height = 64;
 var hero_width = 42;
 var black_holes_used = 0;
 var game_active = false;
-var sensitivity = 10; //higher numbers, slower game
+var sensitivity = 100; //higher numbers, slower game
+var black_hole_weight = 10;
 $(document).ready(function() {
-    /*
-    $(document).click(function(event) {
-       //alert("You clicked me! " + event.pageX + " " + event.pageY);
-        $("#hero_div").css("top",event.pageY+"px");
-        $("#hero_div").css("left",event.pageX+"px");
-    });
-    */
 
     $(document).click(function(event) {
         if (game_active) {
@@ -43,18 +37,9 @@ $(document).ready(function() {
     world.people = [];
     world.collectables = [];
     world.obstacles = [];
-    /*
-    world.people.push({el: $("#extra1"), top: 120, left: 0, directionX: 1, directionY: 1});
-    world.people.push({el: $("#extra2"), top: 0, left: 0, directionX: 1, directionY: 1});
-    world.people.push({el: $("#extra3"), top: 100, left: 300, directionX: 1, directionY: 1});
-    world.people.push({el: $("#extra4"), top: 100, left: 200, directionX: 1, directionY: 1});
-    */
 
     //setStage(1);
 
-    //createCollectables(5);
-    //movePeople();
-    //moveHero();
 });
 
 
@@ -62,14 +47,19 @@ function moveHero() {
     var moveX = 0;
     var moveY = 0;
 
+    //need to be additive for multiple black holes
     for (i in black_holes) {
         if (typeof(black_holes[i]) != "undefined") {
             var total_distance = Math.sqrt(Math.abs(hero_top-black_holes[i].top)^2+Math.abs(hero_left-black_holes[i].left)^2);
             var amplification_effect = (1/((total_distance+10)^2));
             var yVelocity = (black_holes[i].top-hero_top) * amplification_effect;
             var xVelocity = (black_holes[i].left-hero_left) * amplification_effect;
-            moveX = (xVelocity == NaN) ? 0 : xVelocity;
-            moveY = (xVelocity == NaN) ? 0 : yVelocity;
+            //TODO: Update velocity to approproiately deal with sign (+/-) but be based off a black hole "weight"
+
+            //var yVelocity = black_hole_weight * amplification_effect;
+            //var xVelocity = black_hole_weight * amplification_effect;
+            moveX += (xVelocity == NaN) ? 0 : xVelocity;
+            moveY += (xVelocity == NaN) ? 0 : yVelocity;
         }
     }
 
@@ -82,8 +72,7 @@ function moveHero() {
             && checkItem.top + hero_height + hero_velocity_y >= hero_top
             && checkItem.left + hero_width >= hero_left + hero_velocity_x
             && checkItem.left <= hero_left + hero_width + hero_velocity_x
-        )
-        {
+        ) {
             //is the collision from moving left
             if (
                 (hero_left + hero_width < checkItem.left)
@@ -126,16 +115,11 @@ function moveHero() {
                     hero_top = checkItem.top + checkItem.height;
                 }
 
-
                 console.log("Collission detected, but by golly we didn't know what to do with it...");
             }
-
-
             //is the collision from moving DOWN
-
         }
     }
-
 
     var new_hero_top = hero_velocity_y + hero_top;
     var new_hero_left = hero_velocity_x + hero_left;
@@ -225,87 +209,8 @@ function setStage(stage_num) {
     }
     $("#hero_div").css("top",hero_top+"px");
     $("#hero_div").css("left",hero_left+"px");
-    moveHero();
     setTimeout(function() { game_active = true; }, 100);
-}
-
-function createCollectables(num) {
-    for (var i=0; i<=num; i++) {
-        $("#gameboard").append("<div id='collectable"+collectableCount+"' class='collectable'></div>");
-        var top = Math.random()*340;
-        var left = Math.random()*540;
-        $("#collectable"+i).css("left",left+"px");
-        $("#collectable"+i).css("top",top+"px");
-        world.collectables.push({el: $("#collectable"+collectableCount), top: top, left: left});
-        collectableCount++;
-    }
-}
-
-function movePeople() {
-    for (num in world.people) {
-        var person = world.people[num];
-
-        var original_top = person.top;
-        var original_left = person.left;
-
-        person.top = person.top + (parseInt(Math.random()*10)*person.directionY);
-        person.left = person.left + (parseInt(Math.random()*10)*person.directionX);
-
-        if (checkCollision(person, num)) {
-            person.top = original_top;
-            person.left = original_left;
-        }
-
-
-        checkCollectableCollision(person);
-
-        if (Math.random()>.95) {
-            person.directionY = -person.directionY;
-        }
-
-        if (Math.random()>.95) {
-            person.directionX = -person.directionX;
-        }
-
-        if (person.left < 5) { person.left = 5; }
-        if (person.left > 540) { person.left = 540; }
-        if (person.top < 5) { person.top = 5; }
-        if (person.top > 340) { person.top = 340; }
-
-        /*
-        if (checkCollision(person, num)) {
-            console.log("Things are hitting");
-            //world.people[other].top -= 20;
-            person.top += personHeight;
-            //world.people[other].left -= 20;
-            person.left += personWidth;
-        }
-        */
-
-        person.el.css("top",person.top+"px");
-        person.el.css("left",person.left+"px");
-    }
-
-    setTimeout(movePeople, 500);
-}
-
-function checkCollectableCollision(person) {
-    for (item in world.collectables) {
-        var checkItem = world.collectables[item];
-        if (checkItem.top <= person.top + personHeight
-            && checkItem.top + personHeight >= person.top
-            && checkItem.left + personWidth >= person.left
-            && checkItem.left <= person.left + personWidth
-        )
-        {
-            person.el.append("<div class='collected_item'></div>");
-            checkItem.el.remove();
-            world.collectables.splice(item,1);
-            createCollectables(1);
-
-        }
-    }
-
+    moveHero();
 }
 
 function checkCollision(person, num) {
